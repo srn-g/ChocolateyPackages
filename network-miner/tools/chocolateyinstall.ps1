95f43d9ea7b8a49402e56fb59e69435d00372eb2
srn-g <sajad.ara@gmail.com>
2020-05-21 16:17:53 +0430

    Optimize it

--- network-miner/tools/chocolateyinstall.ps1
+++ network-miner/tools/chocolateyinstall.ps1
@@ -2,21 +2,17 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = 'https://www.netresec.com/?download=NetworkMiner'
-$url64 = ''
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
-  fileType       = 'EXE'
+  fileType       = 'ZIP'
   url            = $url
-  url64bit       = $url64
 
   softwareName   = 'network-miner*'
 
-  checksum       = ''
+  checksum       = '3f11dc812a9a3c84fafa29660e6142464582825569771e1f60bd6568e0269b23'
   checksumType   = 'sha256'
-  checksum64     = ''
-  checksumType64 = 'sha256'
 
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0, 3010, 1641)
--- network-miner/tools/chocolateyuninstall.ps1
+++ network-miner/tools/chocolateyuninstall.ps1
@@ -2,7 +2,7 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'network-miner*'
-  fileType       = 'EXE_MSI_OR_MSU'
+  fileType       = 'ZIP'
   silentArgs     = "/qn /norestart"
   validExitCodes = @(0, 3010, 1605, 1614, 1641)
 }
