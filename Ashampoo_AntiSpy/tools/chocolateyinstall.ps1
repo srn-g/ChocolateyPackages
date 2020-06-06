548321a9b9dd155a06622b4304eeb00a015a7a19
srn-g <sajad.ara@gmail.com>
2020-06-06 20:55:03 +0430

    Update package

--- Ashampoo_AntiSpy/tools/chocolateyinstall.ps1
+++ Ashampoo_AntiSpy/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = 'https://cdn1.ashampoo.net/public/ashf/1004/Ashampoo_AntiSpy.exe'
 
-Get-ChocolateyWebFile -PackageName $env:ChocolateyPackageName -FileFullPath "$toolsDir\Ashampoo_AntiSpy.exe" -Url $url
+Get-ChocolateyWebFile -PackageName $env:ChocolateyPackageName -FileFullPath "$toolsDir\Ashampoo_AntiSpy.exe" -Url $url -Checksum "4EDCFFCFADB0AF69C1563B05DF9A33B1C2803F0A37DB8FA73C8B172EA7E886F9" -ChecksumType "sha256"
 
 Install-ChocolateyShortcut `
   -ShortcutFilePath "$([Environment]::GetFolderPath('CommonStartMenu'))\Programs\Ashampoo_AntiSpy.lnk" `
